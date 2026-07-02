985b0070a3bf81466a217ce387e898b19506e952
Enomine-ndrt <luis.w0587@gmail.com>
2026-07-02 16:47:49 -0600

    rest template - feign client

---
+++ usuario-service/src/main/java/com/usuario_service/feignclients/CarroFeignClient.java
@@ -0,0 +1,19 @@
+package com.usuario_service.feignclients;
+
+import com.usuario_service.modelos.Carro;
+import com.usuario_service.modelos.Moto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@FeignClient(name = "carro-service", url = "http://localhost:8002", path = "/carro")
+public interface CarroFeignClient {
+
+    @PostMapping
+    public Carro save(@RequestBody Carro carro);
+
+    @GetMapping("/usuario/{usuarioId}")
+    public List<Carro> getCarros(@PathVariable("usuarioId")int usuarioId);
+
+}
---
+++ usuario-service/src/main/java/com/usuario_service/feignclients/MotoFeignClient.java
@@ -0,0 +1,19 @@
+package com.usuario_service.feignclients;
+
+import com.usuario_service.modelos.Moto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@FeignClient(name = "moto-service", url = "http://localhost:8003", path = "/moto")
+public interface MotoFeignClient {
+
+    @PostMapping
+    public Moto save(@RequestBody Moto moto);
+
+    @GetMapping("/usuario/{usuarioId}")
+    public List<Moto> getMotos(@PathVariable("usuarioId")int usuarioId);
+
+
+}
